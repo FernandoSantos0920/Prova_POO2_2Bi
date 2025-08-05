80fcb0d25d6aec29894273209b36f9b5c5def0ab
FernandoSantos0920 <fernando.santos2@escolar.ifrn.edu.br>
2025-08-05 16:21:15 -0300

    Todos os paineis criados

---
+++ Prova2POO/Assets/Menu.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject panelBase;
+    public GameObject panelOptions;
+    public GameObject panelVideo;
+    public GameObject panelSom;
+    public GameObject panelExtras;
+    public GameObject panelCredits;
+    public GameObject panelControles;
+    
+    
+    public void Play()
+    {
+        SceneManager.LoadScene("Jogo");
+    }
+
+    public void Options()
+    {
+        panelOptions.SetActive(true);
+        panelBase.SetActive(false);
+    }
+
+    public void VoltarOptions()
+    {
+        panelOptions.SetActive(false);
+        panelBase.SetActive(true);
+    }
+
+
+    public void Videos()
+    {
+        panelVideo.SetActive(true);
+        panelOptions.SetActive(false);
+    }
+
+    public void VoltarVideo()
+    {
+        panelVideo.SetActive(false);
+        panelOptions.SetActive(true);
+    }
+
+    public void Som()
+    {
+        panelSom.SetActive(true);
+        panelOptions.SetActive(false);
+    }
+
+    public void VoltarSom()
+    {
+        panelSom.SetActive(false);
+        panelOptions.SetActive(true);
+    }
+
+    public void Controles()
+    {
+        panelControles.SetActive(true);
+        panelOptions.SetActive(false);
+    }
+
+    public void VoltarCOntroles()
+    {
+        panelControles.SetActive(false);
+        panelOptions.SetActive(true);
+    }
+    
+}
