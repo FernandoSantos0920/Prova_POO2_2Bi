9e2c4e5fa6a71905b0688bafd87a92b413f8b8ee
FernandoSantos0920 <fernando.santos2@escolar.ifrn.edu.br>
2025-08-05 22:27:54 -0300

    Inicio das barras

---
+++ Prova2POO/Assets/Scripts/Player Movement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+
+    private int velocidade = 4;
+    void Start()
+    {
+        
+    }
+
+  
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.position -= new Vector3(velocidade * Time.deltaTime, 0, 0);
+        }
+        
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.position += new Vector3(velocidade * Time.deltaTime, 0, 0);
+        }
+        
+        
+    }
+}
